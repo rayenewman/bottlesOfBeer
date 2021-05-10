4d8fcf73785e6de2df01bbe42e2fe85a2487e3dd
rayenewman <rayenewman@outlook.com>
2021-05-10 21:03:54 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numberOfBeers = 99;
+        String bottles = "bottles of beer";
+
+        while (numberOfBeers > 0) {
+            System.out.println(numberOfBeers +" "+ bottles + "on the wall, " + numberOfBeers + " of beer");
+            numberOfBeers--;
+
+            System.out.println("take one down, pass it around " + numberOfBeers + " " + bottles + " on the wall");
+
+        }             System.out.println("No more bottles of beer on the wall");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
